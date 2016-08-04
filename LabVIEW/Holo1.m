d70705f5b95ccd5d92408d86039d0b353c7026c9
Rosenberg, Tanner J (397G-Affiliate) <Tanner.J.Rosenberg@jpl.nasa.gov>
2016-08-04 11:49:44 -0700

    Initial

---
+++ LabVIEW/Holo1.m
@@ -0,0 +1,81 @@
+
+%Hologram and Reconstruction Simulation
+%By: Joel Johnson
+%------------------------------------------------------------------
+
+%Simulate Object
+Image = imread('questionmark1.jpg','bmp'); %imports image Question mark is a BMP!
+Image = double(Image(:,:,1)); %assign image to an array
+figure(1); 
+surf(Image); 
+%colormap(gray(256)); 
+title('Original Image');
+[X,Y] = size(Image); %X and Y are sizes of the image
+for x = 1:X;
+    for y = 1:Y;
+        if Image(x,y) < 1;
+           Image(x,y) = 5;
+           phi(x,y) = pi/2;
+        else Image(x,y) > 1;
+            phi(x,y) = pi;
+        end
+    end
+end
+z = 1e-6;
+lambda = 633e-6;
+
+%Phase Shift of Image
+Objwave = abs(Image).*exp(1i.*phi) + (0.01*lambda)*rand(256);
+figure(2); 
+imagesc(real(Objwave)); %displays real portion of image
+colormap(gray(256)); 
+title('Object Wave');
+
+%Reference Wave
+x = 1:1:X; %makes array
+y = 1:1:Y; %makes array
+[kx,ky] = meshgrid(x,y);
+Referencewave = abs(Image).*exp(1i*(kx + ky)) + (0.01*lambda)*rand(256);
+
+%Simulate Hologram
+H = (abs(Objwave)).^2 + (abs(Referencewave)).^2 + (conj(Objwave).*Referencewave) + (conj(Referencewave).*Objwave);
+Imax = max(max(H)); 
+Ih = uint8(255*H/Imax);
+FIh = (fftshift(fft2(Ih)));
+figure(3); 
+imagesc(H);
+colormap(gray(256));
+title('Hologram');
+figure(4); 
+imagesc(abs(log(FIh))); 
+colormap(gray(256)); 
+title('Fourier Space');
+
+%Simulate Fourier Mask
+Cursorx = 88;
+Cursory = 88;
+filtersize = 10;
+W = zeros(X,Y);
+By = (Cursorx - filtersize:Cursorx + filtersize);
+Bx = (Cursory - filtersize:Cursory + filtersize);
+W(round(Bx),round(By)) = 1;
+FIhfilter = FIh.*W;
+Bx = (X/2) - Cursorx;
+By = (Y/2) - Cursory;
+FIhfilter2 = circshift(FIhfilter,[round(By),round(Bx)]);
+figure(5); %centers around real term (top left)
+imagesc(real(FIhfilter2)); 
+colormap(gray(256)); 
+title('Fourier Space with Filter');
+
+%Hologram Reconstruction using the Angular Spectrum Method
+k = 2*pi/lambda;
+E = exp(1i*z*sqrt((k)^.2 - (kx.*ky) - (ky.*ky)));
+E = (ifft2(FIhfilter2.*E));
+figure(9); 
+surf(abs(E));  
+title('Reconstructed Hologram Angular Spectrum');
+figure(8); 
+imagesc(angle(E)); 
+colormap(gray(256));
+title('Reconstructed Hologram Phase');
